6ad0b8930466f89bfbf4304fb230f9436785e99f
Meelis Lehtmets <meelis@lehtmets.com>
2017-03-29 14:21:49 +0300

    spring boot and dependency management

---
+++ src/main/java/ee/iglu/sample/Main.java
@@ -0,0 +1,9 @@
+package ee.iglu.sample;
+
+import org.springframework.boot.SpringApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
