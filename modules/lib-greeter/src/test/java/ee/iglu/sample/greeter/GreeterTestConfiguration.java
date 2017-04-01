ae0c976f2c709fde345335205e31cb936e91ee37
Meelis Lehtmets <meelis@lehtmets.com>
2017-04-01 05:36:59 +0300

    add tests to greeter

---
+++ modules/lib-greeter/src/test/java/ee/iglu/sample/greeter/GreeterTestConfiguration.java
@@ -0,0 +1,9 @@
+package ee.iglu.sample.greeter;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@SpringBootConfiguration
+class GreeterTestConfiguration {
+}
