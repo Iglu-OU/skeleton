cce3ced3df1b4148a10d1f899494f5a716e987e8
Meelis Lehtmets <meelis@lehtmets.com>
2017-06-26 20:15:59 +0300

    skeleton: move libs to separate folder

--- libs/rpc/src/main/java/ee/iglu/skeleton/rpc/impl/CustomJacksonModule.java
+++ libs/rpc/src/main/java/ee/iglu/skeleton/rpc/impl/CustomJacksonModule.java
@@ -1,17 +1,15 @@
 package ee.iglu.skeleton.rpc.impl;
 
-import java.io.IOException;
-import java.time.Instant;
-
-import javax.annotation.PostConstruct;
-
-import org.springframework.stereotype.Component;
-
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.time.Instant;
 
 @Component
 class CustomJacksonModule extends SimpleModule {
